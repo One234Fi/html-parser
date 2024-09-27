9c119e9baf53a8f7cfaf3c996754151b1f7fbebd
One234Fi <54912519+One234Fi@users.noreply.github.com>
2024-09-26 23:42:29 -0500

    macros for logging and assert

--- src/common.h
+++ src/common.h
@@ -3,6 +3,17 @@
 
 #include <stdlib.h>
 
+#define RED         "\x1b[31m"
+#define GREEN       "\x1b[32m"
+#define YELLOW      "\x1b[33m"
+#define BLUE        "\x1b[36m"
+#define COLOR_RESET "\x1b[0m"
+
+#define LOG_INFO(message) fprintf(stderr, BLUE"%s, %s, %d:"COLOR_RESET" \"%s\"\n", __FILE__, __func__, __LINE__, message)
+#define LOG_ERROR(message) fprintf(stderr, BLUE"%s, %s, %d:"RED" \"%s\"\n"COLOR_RESET, __FILE__, __func__, __LINE__, message)
+#define LOG_WARN(message) fprintf(stderr, BLUE"%s, %s, %d:"YELLOW" \"%s\"\n"COLOR_RESET, __FILE__, __func__, __LINE__, message)
+#define ASSERT(expr, message, return_val) if (expr) { LOG_ERROR(message); return return_val; } 
+
 #define ALLOCATE(ptr, newsize) ptr = realloc(ptr, newsize)
 
 #endif
--- test/test.h
+++ test/test.h
@@ -2,12 +2,7 @@
 #define TEST_MODULE
 
 #include <string.h>
-
-#define RED         "\x1b[31m"
-#define GREEN       "\x1b[32m"
-#define YELLOW      "\x1b[33m"
-#define BLUE        "\x1b[34m"
-#define COLOR_RESET "\x1b[0m"
+#include "common.h"
 
 extern size_t succeeded;
 extern size_t failed;
