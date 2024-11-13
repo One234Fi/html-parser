ee974faa7b6a726c4e03898c4dda988588186fc6
One234Fi <54912519+One234Fi@users.noreply.github.com>
2024-11-12 23:51:40 -0600

    better generic vector

--- src/vector.c
+++ src/vector.c
@@ -63,5 +63,33 @@ vector_get(const vector * v, const size_t index) {
     return v->data + v->item_size * index;
 }
 
+void grow(void * slice, ptrdiff_t size, ptrdiff_t align, arena * a) {
+    struct {
+        void * data;
+        ptrdiff_t len;
+        ptrdiff_t cap;
+    } temp;
+    memcpy(&temp, slice, sizeof(temp));
+    assert(temp.len >= 0);
+    assert(temp.cap >= 0);
+    assert(temp.len <= temp.cap);
+
+    if (!temp.data) {
+        temp.cap = 1;
+        temp.data = alloc(a, size * 2, align, temp.cap);
+    }
+    else if (a->beg == temp.data + size * temp.cap) {
+        alloc(a, size, 1, temp.cap); 
+    }
+    else {
+        void * data = alloc(a, size * 2, align, temp.cap);
+        memcpy(data, temp.data, size * temp.len);
+        temp.data = data;
+    }
+
+    temp.cap *= 2;
+    memcpy(slice, &temp, sizeof(temp));
+}
+
 #undef VEC_DEFAULT_SIZE
 #undef VEC_GROWTH_FACTOR
--- src/vector.h
+++ src/vector.h
@@ -1,8 +1,29 @@
 #ifndef VECTOR_MODULE
 #define VECTOR_MODULE
 
-#include <stdio.h>
+/* vector definition
+ *
+ *  struct {
+ *      void * data;
+ *      ptrdiff_t len;
+ *      ptrdiff_t cap;
+ *  };
+ */
 
+#include "arena.h"
+#include <assert.h>
+#include <stddef.h>
+
+void grow(void * slice, ptrdiff_t size, ptrdiff_t align, arena * a);
+
+#define push(s, arena) (\
+    (s)->len < (s)->cap ? \
+        (s)->data + (s)->len++ : \
+        grow(s, sizeof(*(s)->data), _Alignof(*(s)->data), arena), (s)->data + (s)->len++ \
+) 
+
+
+//TODO delete/replace this
 typedef struct vector vector;
 
 vector * vector_init(size_t item_size);
@@ -10,5 +31,6 @@ void vector_destroy(vector * v);
 void vector_push_back(vector * v, void * item);
 const void * vector_pop_back(vector * v);
 const void * vector_get(const vector * v, const size_t index);
+//TODO delete/replace this
 
 #endif
